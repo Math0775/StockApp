65e75557453633a8f70573ea0aebd11b83921984
Matheus <matheusandradepompermayer07@gmail.com>
2023-12-01 21:53:12 -0300

    Implementando a string de conexão SQL.

---
+++ Source/DataAcess/StockApp.DataAcess/Database/SqlConexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.DataAcess.Database
+{
+    internal static class SqlConexao
+    {
+        internal static string ConexaoComBanco => @"Data Source=.\SQLEXPRESS;Initial Catalog=StockPhone;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+        internal static string ConexaoSemBanco => @"Data Source=.\SQLEXPRESS;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+    }
+}
